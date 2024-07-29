439244c21f3e3d95a3ff097788c372645dd25643
Tyler <psenk168@gmail.com>
2024-07-28 23:49:53 -0500

    bug, missing await

--- ApproveTool.py
+++ ApproveTool.py
@@ -46,7 +46,7 @@ class ApproveTool(discord.ui.View):
         """
         submission = self.submissions[self.page]
         approve_tool = discord.Embed(title=f"Submission Approval Tool", color=0xFFFF00)
-        msg = self.get_message(submission['message_id'], submission['team'])
+        msg = await self.get_message(submission['message_id'], submission['team'])
         approve_tool.set_image(url=msg.attachments[0].url)
         approve_tool.add_field(name="Submission", value=f"[HERE]({submission['jump_url']})", inline=True)
         approve_tool.add_field(name="Player", value=f"{submission['player']}", inline=True)
